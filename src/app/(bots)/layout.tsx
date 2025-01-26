6839a1c17ee33024be0328c13e0a99077f204838
MariaJyate <maria.jyate@gmail.com>
2025-01-26 20:37:37 +0100

    menu fix(?)

--- src/app/(bots)/layout.tsx
+++ src/app/(bots)/layout.tsx
@@ -8,8 +8,8 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
   return (
-    <div className="flex h-full flex-col overflow-hidden md:flex-row bg-white">
-        <div className="w-full flex-none md:w-64">
+    <div className="flex h-full flex-col justify-between overflow-hidden md:flex-row bg-white">
+        <div className="w-full md:w-64">
             <Menu/>
         </div>
         <div className="flex-grow p-0 md:pl-12 md:mx-auto">{children}</div>
